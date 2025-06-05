0554fc9fe7f2e340a0d6959e09170f799098d639
Jefte Keller <jeftekeller.dev@gmail.com>
2025-06-05 05:18:27 -0300

    feature: add option to download only the images from the album

--- video_game_music_downloader.py
+++ video_game_music_downloader.py
@@ -1,5 +1,5 @@
 import os
-from parser.page import get_info_from_page
+from parser.page import get_album_name_from_page, get_info_from_page
 
 from common.utils import gen_argparse
 from download.image import download_album_images_from_page
@@ -8,16 +8,22 @@ from download.song import download_songs_from_list
 
 def main() -> None:
     args = gen_argparse()
-
-    album_name, song_links = get_info_from_page(
-        args.album_page_url, args.load_from_file, args.lossy, args.no_lossless
-    )
+    album_name = get_album_name_from_page(args.album_page_url)
 
     output_dir = os.path.join(args.output_path, album_name)
     os.makedirs(output_dir)
 
     if not args.no_images:
         download_album_images_from_page(args.album_page_url, output_dir)
+
+        if args.only_images:
+            return print(
+                f'\nImages from album: "{album_name}" downloaded to "{args.output_path}" successfully!'
+            )
+
+    song_links = get_info_from_page(
+        args.album_page_url, args.load_from_file, args.lossy, args.no_lossless
+    )
 
     download_songs_from_list(song_links, output_dir)
 
